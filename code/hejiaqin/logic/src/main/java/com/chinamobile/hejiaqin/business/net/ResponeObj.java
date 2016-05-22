cfb6ef9bd6a69ea874afdb931fc597929c250027
zhanggj <34401287@qq.com>
2016-05-22 20:11:30 +0800

    增加ResponeObj解析接口

---
+++ code/hejiaqin/logic/src/main/java/com/chinamobile/hejiaqin/business/net/ResponeObj.java
@@ -0,0 +1,11 @@
+package com.chinamobile.hejiaqin.business.net;
+
+import java.util.Objects;
+
+/**
+ * Created by zhanggj on 2016/5/22.
+ */
+public interface ResponeObj {
+
+    Object parseResponeData(String data);
+}
