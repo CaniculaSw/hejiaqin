d35a9bd298dd842013338c57e0a34233821c7716
eshaohu <67786201@qq.com>
2016-05-22 15:57:01 +0800

    Just for test

--- code/hejiaqin/phone/src/main/java/com/chinamobile/hejiaqin/business/ui/main/SettingFragment.java
+++ code/hejiaqin/phone/src/main/java/com/chinamobile/hejiaqin/business/ui/main/SettingFragment.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
 import com.chinamobile.hejiaqin.R;
 import com.chinamobile.hejiaqin.business.BussinessConstants;
