bcc43c6e4c2b7dcc5c92ef41751f95413f2360ef
thsowhat <tonghui19890408@163.com>
2016-05-23 21:49:28 +0800

    联系人列表页面实现: 分组展示联系人.

--- code/hejiaqin/phone/src/main/java/com/chinamobile/hejiaqin/business/ui/contact/fragment/SysContactListFragment.java
+++ code/hejiaqin/phone/src/main/java/com/chinamobile/hejiaqin/business/ui/contact/fragment/SysContactListFragment.java
@@ -5,6 +5,8 @@ import android.view.View;
 
 import com.chinamobile.hejiaqin.R;
 import com.chinamobile.hejiaqin.business.ui.basic.BasicFragment;
+import com.chinamobile.hejiaqin.business.ui.basic.view.stickylistview.StickyListHeadersListView;
+import com.chinamobile.hejiaqin.business.ui.contact.adapter.AppContactAdapter;
 
 /**
  *
@@ -29,7 +31,9 @@ public class SysContactListFragment extends BasicFragment {
 
     @Override
     protected void initView(View view) {
-
+        StickyListHeadersListView stickyList = (StickyListHeadersListView) view.findViewById(R.id.list);
+        AppContactAdapter adapter = new AppContactAdapter(this.getContext());
+        stickyList.setAdapter(adapter);
     }
 
     @Override
