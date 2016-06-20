90607274aa9e8f759e5755a151ce926e02013ec6
thsowhat <tonghui19890408@163.com>
2016-06-20 22:01:31 +0800

    联系人列表页面实现: 增加网络获取联系人逻辑

--- code/hejiaqin/logic/src/main/java/com/chinamobile/hejiaqin/business/logic/contacts/ContactsLogic.java
+++ code/hejiaqin/logic/src/main/java/com/chinamobile/hejiaqin/business/logic/contacts/ContactsLogic.java
@@ -4,8 +4,12 @@ import com.chinamobile.hejiaqin.business.BussinessConstants;
 import com.chinamobile.hejiaqin.business.dbApdater.ContactsDbAdapter;
 import com.chinamobile.hejiaqin.business.manager.ContactsInfoManager;
 import com.chinamobile.hejiaqin.business.model.contacts.ContactsInfo;
+import com.chinamobile.hejiaqin.business.net.IHttpCallBack;
+import com.chinamobile.hejiaqin.business.net.MapStrReqBody;
+import com.chinamobile.hejiaqin.business.net.contacts.ContactsHttpManager;
 import com.customer.framework.component.ThreadPool.ThreadPoolUtil;
 import com.customer.framework.component.ThreadPool.ThreadTask;
+import com.customer.framework.component.net.NetResponse;
 import com.customer.framework.logic.LogicImp;
 
 import java.util.List;
@@ -36,9 +40,45 @@ public class ContactsLogic extends LogicImp implements IContactsLogic {
             @Override
             public void run() {
                 // 从网络获取联系人数据 TODO
+                MapStrReqBody reqBody = new MapStrReqBody();
+                // 携带token TODO
+                // reqBody.add("token", token);
+                new ContactsHttpManager(getContext()).list(null, reqBody, new IHttpCallBack() {
+                    /**
+                     * 网络请求成功响应
+                     *
+                     * @param invoker 调用者(用来区分不同的调用场景，差异化实现回调逻辑)
+                     * @param obj     服务器响应解析后的对象
+                     */
+                    @Override
+                    public void onSuccessful(Object invoker, Object obj) {
+
+                    }
+
+                    /**
+                     * 网络请求业务失败响应
+                     *
+                     * @param invoker 调用者(用来区分不同的调用场景，差异化实现回调逻辑)
+                     * @param code    业务错误码
+                     * @param desc    业务错误描述
+                     */
+                    @Override
+                    public void onFailure(Object invoker, String code, String desc) {
+
+                    }
+
+                    /**
+                     * 网络请求网络失败响应
+                     *
+                     * @param errorCode 网络错误码
+                     */
+                    @Override
+                    public void onNetWorkError(NetResponse.ResponseCode errorCode) {
+
+                    }
+                });
 
                 // 从数据库获取联系人数据
-
                 // 伪造数据,用于测试,先从本地联系人中获取 TODO
                 ContactsDbAdapter.getInstance(getContext(), "aaa").delAll();
                 List<ContactsInfo> contactsInfoList = ContactsInfoManager.getInstance().getLocalContactLst(getContext());
