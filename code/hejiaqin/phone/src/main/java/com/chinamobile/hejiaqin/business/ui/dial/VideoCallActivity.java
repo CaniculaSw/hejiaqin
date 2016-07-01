cbd5fb7c7fab9bbea1770bac9f38a69734897d8f
zhanggj <34401287@qq.com>
2016-07-02 01:25:20 +0800

    通话页面高保真开发

---
+++ code/hejiaqin/phone/src/main/java/com/chinamobile/hejiaqin/business/ui/dial/VideoCallActivity.java
@@ -0,0 +1,35 @@
+package com.chinamobile.hejiaqin.business.ui.dial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chinamobile.hejiaqin.R;
+import com.chinamobile.hejiaqin.business.ui.basic.BasicActivity;
+
+public class VideoCallActivity extends BasicActivity {
+
+    @Override
+    protected void initLogics() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_video_call;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initDate() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
