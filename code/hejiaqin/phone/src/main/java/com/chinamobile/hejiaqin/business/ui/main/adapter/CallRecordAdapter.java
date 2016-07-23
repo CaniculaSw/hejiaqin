bd12909f03b0f6691af3c557096d8e0681d21d50
zhanggj <34401287@qq.com>
2016-07-23 22:30:03 +0800

    1、通话adapter

---
+++ code/hejiaqin/phone/src/main/java/com/chinamobile/hejiaqin/business/ui/main/adapter/CallRecordAdapter.java
@@ -0,0 +1,25 @@
+package com.chinamobile.hejiaqin.business.ui.main.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhanggj on 2016/7/20.
+ */
+public class CallRecordAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ code/hejiaqin/phone/src/main/java/com/chinamobile/hejiaqin/business/ui/main/adapter/DialContactAdapter.java
@@ -0,0 +1,24 @@
+package com.chinamobile.hejiaqin.business.ui.main.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhanggj on 2016/7/20.
+ */
+public class DialContactAdapter extends RecyclerView.Adapter  {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
