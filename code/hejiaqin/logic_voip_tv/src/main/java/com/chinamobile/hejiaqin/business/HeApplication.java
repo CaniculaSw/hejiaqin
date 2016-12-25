ae3fc5982e8f082572b8d194c576f232feb8db3c
zhanggj <34401287@qq.com>
2016-12-26 04:11:04 +0800

    1、根据华为TV开发指南修改代码

--- code/hejiaqin/logic_voip_tv/src/main/java/com/chinamobile/hejiaqin/business/HeApplication.java
+++ code/hejiaqin/logic_voip_tv/src/main/java/com/chinamobile/hejiaqin/business/HeApplication.java
@@ -2,16 +2,22 @@ package com.chinamobile.hejiaqin.business;
 
 import com.chinamobile.hejiaqin.business.logic.setting.SettingLogic;
 import com.chinamobile.hejiaqin.business.logic.voip.VoipLogic;
+import com.chinamobile.hejiaqin.business.utils.DirUtil;
+import com.customer.framework.utils.LogUtil;
 import com.huawei.rcs.RCSApplication;
 import com.huawei.rcs.call.CallApi;
-import com.huawei.rcs.hme.HmeAudio;
+import com.huawei.rcs.call.MediaApi;
+import com.huawei.rcs.hme.HmeAudioTV;
 import com.huawei.rcs.hme.HmeVideo;
-import com.huawei.rcs.login.LoginApi;
 import com.huawei.rcs.message.MessagingApi;
+import com.huawei.rcs.system.DmVersionInfo;
 import com.huawei.rcs.system.SysApi;
 import com.huawei.rcs.tls.DefaultTlsHelper;
 import com.huawei.rcs.upgrade.UpgradeApi;
+import com.huawei.usp.UspCfg;
 import com.littlec.sdk.manager.CMIMHelper;
+
+import java.io.File;
 
 /**
  * Created by  on 2016/6/5.
@@ -20,23 +26,33 @@ public class HeApplication extends RCSApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        String hmeLogPath = DirUtil.getExternalFileDir(getApplicationContext()) + "/log/hme";
+        LogUtil.d(Const.TAG_UI, "The hmelogpath is " + hmeLogPath);
+        File targetDir = new File(hmeLogPath);
+        if (!targetDir.exists())
+        {
+            if (!targetDir.mkdirs())
+            {
+                LogUtil.e(Const.TAG_UI, "mkdir failed: " + hmeLogPath);
+            }
+        }
+        MediaApi.setConfigString(UspCfg.JEN_UMME_CFG_HME_LOGPATH, hmeLogPath + "/");
+
+        SysApi.loadTls(new DefaultTlsHelper());
+
         UpgradeApi.init(getApplicationContext());
-        HmeAudio.setup(this);
+
+        HmeAudioTV.setup(this);
+        String deviceName = getDevice();
+        HmeVideo.setVideoMode(HmeVideo.VIDEO_MODE_STB);
         HmeVideo.setup(this);
         CallApi.init(getApplicationContext());
-        CallApi.setConfig(CallApi.CONFIG_MAJOR_TYPE_VIDEO_DISPLAY_TYPE, CallApi.CONFIG_MINOR_TYPE_DEFAULT, "1");
-        CallApi.setConfig(CallApi.CONFIG_MAJOR_PREVIEW_BEFORE_CONNED,
-                CallApi.CONFIG_MINOR_TYPE_DEFAULT, CallApi.CFG_VALUE_YES);
-        CallApi.setConfig(CallApi.CONFIG_MAJOR_TYPE_SRTP, CallApi.CONFIG_MINOR_TYPE_SRTP_ALL, CallApi.CFG_CALL_ENABLE_SRTP);
-//        LoginApi.setConfig(LoginApi.CONFIG_MAJOR_TYPE_TPT_TYPE, LoginApi.CONFIG_MINOR_TYPE_DEFAULT, LoginApi.VALUE_MAJOR_TYPE_TPT_TLS);
-        LoginApi.setConfig(LoginApi.CONFIG_MAJOR_TYPE_KEEP_ALIVE_RSP_TIMER_LEN ,LoginApi.CONFIG_MINOR_TYPE_DEFAULT,"5");
-        LoginApi.setConfig(LoginApi.CONFIG_MAJOR_TYPE_USE_IPV6 ,LoginApi.CONFIG_MINOR_TYPE_DEFAULT,"1");
-        //设置不插入系统通话记录
-        CallApi.setCustomCfg(CallApi.CFG_CALLLOG_INSERT_SYS_DB, CallApi.CFG_VALUE_NO);
-        SysApi.loadTls(new DefaultTlsHelper());
-//        SysApi.loadStg(new NatStgHelper());
-//        SysApi.loadStg(new SvnStgHelper());
-        SysApi.setDMVersion("V1.2.88.5-02230000");
+        DmVersionInfo  versionInfo = new DmVersionInfo("V1.0.0.96", SysApi.VALUE_MAJOR_TYPE_PLATFORM_STB,
+                SysApi.VALUE_MAJOR_TYPE_OS_ANDROID, SysApi.VALUE_MAJOR_TYPE_APP_RCS, "00");
+        SysApi.setDMVersion(versionInfo);
+        CallApi.setConfig(CallApi.CONFIG_MAJOR_DEVICE_NAME, CallApi.CONFIG_MINOR_TYPE_DEFAULT, deviceName);
+        CaaSSdkService.setVideoLevel(0);
+
         //initial message API
         MessagingApi.init(getApplicationContext());
         MessagingApi.setAllowSendDisplayStatus(true);
@@ -46,12 +62,6 @@ public class HeApplication extends RCSApplication {
                 MessagingApi.CONFIG_MAJOR_USE_SYS_SMS,
                 MessagingApi.CONFIG_MINOR_TYPE_DEFAULT, "0");
 
-//        CaasOmp.init();
-//        CaasOmpCfg.setString(CaasOmpCfg.EN_OMP_CFG_SERVER_IP, "205.177.226.80");
-//        CaasOmpCfg.setUint(CaasOmpCfg.EN_OMP_CFG_SERVER_PORT, 8543);
-       /* UI must set  the interface for safety certification.
-       see detail in Developer's Guide*/
-        // SysApi.setTrustCaFilePath("/rootcert.pem");
         VoipLogic.getInstance(getApplicationContext()).registerVoipReceiver();
         SettingLogic.getInstance(getApplicationContext()).registerMessageReceiver();
         /**
@@ -60,6 +70,46 @@ public class HeApplication extends RCSApplication {
         CMIMHelper.getCmAccountManager().init(getApplicationContext(), BussinessConstants.CommonInfo.LITTLEC_APP_KEY);
     }
 
+    public String getDevice()
+    {
+        String sDevice = android.os.Build.DEVICE;
+        String sModel = android.os.Build.MODEL;
+        LogUtil.d(Const.TAG_UI, "device=" + sDevice + "--sModel=" + sModel);
+        if (sDevice.contains("Hi3716CV200"))
+        {
+            Const.DEVICE_TYPE = Const.TYPE_3719C;
+        }
+        else if (sDevice.contains("Hi3719CV100"))
+        {
+            Const.DEVICE_TYPE = Const.TYPE_3719C;
+        }
+        else if (sDevice.contains("Hi3719MV100"))
+        {
+            Const.DEVICE_TYPE = Const.TYPE_3719M;
+        }
+        else if (sDevice.contains("Hi3798MV100"))
+        {
+            Const.DEVICE_TYPE = Const.TYPE_3798M;
+        }
+        else
+        {
+            LogUtil.e(Const.TAG_UI, "the device is Other!");
+            Const.DEVICE_TYPE = Const.TYPE_OTHER;
+        }
+        String deviceName = null;
+        if (Const.TYPE_3798M == Const.DEVICE_TYPE)
+        {
+            deviceName = "STB_3798M";
+        }
+        else if (Const.TYPE_3719C == Const.DEVICE_TYPE || Const.TYPE_3719M == Const.DEVICE_TYPE)
+        {
+            deviceName = "STB_3719C";
+        }
+        else{
+            deviceName = sDevice;
+        }
+        return deviceName;
+    }
 
     @Override
     public void onTerminate() {
